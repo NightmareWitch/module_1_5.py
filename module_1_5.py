cc625f19dd92a34e8fc40d2f300eb2becb9bbd31
NightmareWitch <jokerhell13@gmail.com>
2024-08-23 11:12:55 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,20 @@
+immutable_var = (5, 8, 'd', 'dog')
+print(immutable_var)
+immutable_var = ([7, 15], 'd', 'dog')
+print(immutable_var)
+immutable_var = (5, 8, 'd', 'dog') + (57, 'cat')
+print(immutable_var)
+immutable_var = (5, 8, 'd', 'dog') * 7
+print(immutable_var)
+
+mutable_list = [25, 8, 'a', 'morning', 'friday']
+print(mutable_list)
+print(mutable_list[2])
+mutable_list [2] = 'today'
+print(mutable_list)
+mutable_list.extend(['2025','summer'])
+print(mutable_list)
+mutable_list.remove(8)
+print(mutable_list)
+print('dog' in mutable_list)
+print(5 not in mutable_list)
